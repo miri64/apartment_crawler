97b4408dd1cf243da9219f9e0a23f386ccbfa21c
Authmillenon <authmill@datalove.me>
2011-10-02 22:11:45 +0200

    Definition of Address model

--- crawler/models.py
+++ crawler/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+class Address(models.Model):
+    street = models.CharField(max_length=20)
+    number = models.CharField(max_length=5)
+    nation = models.CharField(max_length=3)
+    state = models.CharField(max_length=30)
+    zip_code = models.CharField(max_length=10)
+    city = models.CharField(max_length=30)
