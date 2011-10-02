e5cb81d3180d5094cc9f56976128daa6e05a26d2
Authmillenon <authmill@datalove.me>
2011-10-02 22:09:51 +0200

    ExposeParserFactory implemented

--- crawler/expose_parser.py
+++ crawler/expose_parser.py
@@ -10,3 +10,33 @@ class ExposeParser():
     def __init__():
         self.pyquery = PyQuery(lxml.html.parse(expose_link).getroot())
 
+class ImmonetExposeParser(ExposeParser):
+    pass    # dummy
+
+class ImmoscoutExposeParser(ExposeParser):
+    pass    # dummy
+
+class ImmoweltExposeParser(ExposeParser):
+    pass    # dummy
+
+class ExposeParserFactory():
+    # __new__ and __init__ make this class a singleton
+    # Source: http://de.wikipedia.org/wiki/Singleton_(Entwurfsmuster)#Implementierung_in_Python_.28ab_Version_2.2.29
+    def __new__(type, *args):
+        if not '_the_instance' in type.__dict__:
+            type._the_instance = object.__new__(type)
+        return type_the_instance
+    
+    def __init__(self):
+        if not '_ready' in dir(self):
+            self._ready = True
+    
+    def get_expose_parser(self, expose_link):
+        if expose_link != None:
+            if expose_link.find('immobilienscout24.de') >= 0:
+                return ImmoscoutExposeParser(expose_link)
+            elif expose_link.find('immonet.de') >= 0:
+                return ImmonetExposeParser(expose_link)
+            elif expose_link.find('immowelt.de') >= 0:
+                return ImmoweltExposeParser(expose_link)
+        raise Exception("Illegal Link: "+expose_link)
