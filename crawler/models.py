a20a1ec4cc39637eaceb92683dc82b76a9a903ed
Authmillenon <authmill@datalove.me>
2011-10-05 01:17:03 +0200

    Allow None-Values in DB

--- crawler/models.py
+++ crawler/models.py
@@ -5,12 +5,12 @@ from datetime import timedelta, date
 import expose_parser
 
 class Address(models.Model):
-    street = models.CharField(max_length=20)
-    number = models.CharField(max_length=5)
-    nation = models.CharField(max_length=3)
-    state = models.CharField(max_length=30)
-    zip_code = models.CharField(max_length=10)
-    city = models.CharField(max_length=30)
+    street = models.CharField(max_length=20, default='')
+    number = models.CharField(max_length=5, default='')
+    nation = models.CharField(max_length=3, default='')
+    state = models.CharField(max_length=30, default='')
+    zip_code = models.CharField(max_length=10, default='')
+    city = models.CharField(max_length=30, default='')
     
     class Meta:
         unique_together = (
@@ -18,21 +18,12 @@ class Address(models.Model):
             )
 
 class Contact(Address):
-    name = models.CharField(max_length=30)
-    phone = models.CharField(max_length=30)
-    mobile = models.CharField(max_length=30)
-    fax = models.CharField(max_length=30)
-    web = models.CharField(max_length=100)
-    mail = models.EmailField()
-    
-    class Meta:
-        unique_together = (
-                (
-                    'name','street','number','nation','state',
-                    'zip_code','city','phone','mobile','fax','web',
-                    'mail'
-                )
-            )
+    name = models.CharField(max_length=30, default='')
+    phone = models.CharField(max_length=30, default='')
+    mobile = models.CharField(max_length=30, default='')
+    fax = models.CharField(max_length=30, default='')
+    web = models.CharField(max_length=100, default='')
+    mail = models.EmailField(default='')
 
 class ExposeManager(models.Manager):
     MAXIMUM_EXPOSE_AGE = timedelta(days=3)
@@ -49,24 +40,24 @@ class Expose(models.Model):
     objects = ExposeManager()
     title = models.CharField(max_length=200)
     expose_link = models.CharField(max_length=100, primary_key=True)
-    address = models.ForeignKey(Address, blank=True)
-    contact = models.ForeignKey(Contact, related_name='offered_expose_set', blank=True)
-    cold_rent = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
-    additional_charges = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
-    operation_expenses = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
-    heating_cost = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
-    total_rent = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
-    heating_type = models.CharField(max_length=20, blank=True)
-    object_state = models.CharField(max_length=30, blank=True)
-    security = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
-    commission = models.CharField(max_length=30, blank=True)
-    space = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
-    floor = models.IntegerField(blank=True)
-    flat_type = models.CharField(max_length=30, blank=True)
-    rooms = models.DecimalField(max_digits=4, decimal_places=2, blank=True)
-    year = models.IntegerField(blank=True)
-    availability = models.CharField(max_length=30, blank=True)
-    last_modified = models.DateField(auto_now=True, blank=True)
+    address = models.ForeignKey(Address, blank=True, null=True)
+    contact = models.ForeignKey(Contact, related_name='offered_expose_set', blank=True, null=True)
+    cold_rent = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
+    additional_charges = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
+    operation_expenses = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
+    heating_cost = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
+    total_rent = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
+    heating_type = models.CharField(max_length=20, blank=True, null=True)
+    object_state = models.CharField(max_length=30, blank=True, null=True)
+    security = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
+    commission = models.CharField(max_length=30, blank=True, null=True)
+    space = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
+    floor = models.IntegerField(blank=True, null=True)
+    flat_type = models.CharField(max_length=30, blank=True, null=True)
+    rooms = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+    availability = models.CharField(max_length=30, blank=True, null=True)
+    last_modified = models.DateField(auto_now=True, blank=True, null=True)
     
     @staticmethod
     def get_expose_by_link(expose_link):
